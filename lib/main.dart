5557280b2457c0c860638df9545e60adbadbf9c1
Rhys Jones (REJ) <RhysEdward.Jones@wales.nhs.uk>
2020-01-27 15:35:33 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Checklist',
+    );
+  }
+}
