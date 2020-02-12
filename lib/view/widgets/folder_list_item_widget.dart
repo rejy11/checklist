7c7e9874b28b1bfe9611226b0dadfa233d11d272
Rhys Jones (REJ) <RhysEdward.Jones@wales.nhs.uk>
2020-02-12 11:05:31 +0000

    final commit

--- lib/view/widgets/folder_list_item_widget.dart
+++ lib/view/widgets/folder_list_item_widget.dart
@@ -1,10 +1,9 @@
-import 'package:circular_check_box/circular_check_box.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
 
 import '../../models/folder_model.dart';
 import '../../providers/folders_provider.dart';
+import 'folder_list_tile.dart';
 
 class FolderListItemWidget extends StatefulWidget {
   final FolderModel folder;
@@ -47,90 +46,39 @@ class _FolderListItemWidgetState extends State<FolderListItemWidget>
         ),
         margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
         elevation: 1,
-        child: Row(
-          children: [
-            Container(
-              color: widget.folder.isFavourite
-                  ? Theme.of(context).accentColor
-                  : Colors.transparent,
-              height: 70,
-              width: 7,
-            ),
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(15),
-                  bottomLeft: Radius.circular(15),
-                  bottomRight: Radius.circular(15),
-                ),
-                child: ListTile(
-                  selected: false,
-                  title: Text(widget.folder.name),
-                  onTap: () {},
-                  onLongPress: widget.onLongPress,
-                  trailing: _buildListItemTrailing(),
-                  leading: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: 50,
-                        child: Consumer<FoldersProvider>(
-                          builder: (BuildContext context, FoldersProvider value,
-                              Widget child) {
-                            if (value.deleteFolderMode) {
-                              return CircularCheckBox(
-                                value: value.isFolderChecked(widget.folder.id),
-                                onChanged: (value) {
-                                  Provider.of<FoldersProvider>(context,
-                                          listen: false)
-                                      .toggleFolderDeleteCheckbox(
-                                          widget.folder.id, value);
-                                },
-                              );
-                            } else {
-                              return Icon(
-                                MaterialCommunityIcons.folder_outline,
-                                color: Theme.of(context).accentColor,
-                              );
-                            }
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: widget.folder.numberOfLists != 1
-                      ? Text('${widget.folder.numberOfLists} items')
-                      : Text('${widget.folder.numberOfLists} item'),
-                ),
-              ),
-            ),
-          ],
+        child: FolderListTile(
+          folder: widget.folder,
+          onLongPress: widget.onLongPress,
+          trailing: _buildListItemTrailing(),
         ),
       ),
     );
   }
 
   Widget _buildListItemTrailing() {
-    return PopupMenuButton(
-      itemBuilder: (ctx) {
-        return [
-          PopupMenuItem(
-            child: widget.folder.isFavourite
-                ? Text('Unfavourite')
-                : Text('Favourite'),
-            value: 1,
-          )
-        ];
-      },
-      onSelected: (value) async {
-        if (value == 1) {
-          widget.folder.isFavourite = !widget.folder.isFavourite;
-          Provider.of<FoldersProvider>(context, listen: false)
-              .updateFolder(widget.folder);
-          await _slideController.forward();
-          await _slideController.reverse();
-        }
-      },
+    return Opacity(
+      opacity: 0.3,
+          child: PopupMenuButton(
+        itemBuilder: (ctx) {
+          return [
+            PopupMenuItem(
+              child: widget.folder.isFavourite
+                  ? Text('Unfavourite')
+                  : Text('Favourite'),
+              value: 1,
+            )
+          ];
+        },
+        onSelected: (value) async {
+          if (value == 1) {
+            widget.folder.isFavourite = !widget.folder.isFavourite;
+            Provider.of<FoldersProvider>(context, listen: false)
+                .updateFolder(widget.folder);
+            await _slideController.forward();
+            await _slideController.reverse();
+          }
+        },
+      ),
     );
   }
 }
---
+++ lib/view/widgets/folder_list_tile.dart
@@ -0,0 +1,113 @@
+import 'package:circular_check_box/circular_check_box.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/folder_model.dart';
+import '../../providers/folders_provider.dart';
+
+class FolderListTile extends StatelessWidget {
+  final FolderModel folder;
+  final Function onTap;
+  final Function onLongPress;
+  final Widget trailing;
+
+  const FolderListTile({
+    this.folder,
+    this.onTap,
+    this.onLongPress,
+    this.trailing,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topRight: Radius.circular(15),
+        bottomLeft: Radius.circular(15),
+        bottomRight: Radius.circular(15),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        onLongPress: onLongPress,
+        child: Row(
+          children: <Widget>[
+            //favourite indicator
+            Container(
+              color: folder.isFavourite
+                  ? Theme.of(context).accentColor
+                  : Colors.transparent,
+              height: 70,
+              width: 7,
+            ),
+            //folder icon/checkbox
+            Container(
+              width: 80,
+              child: Consumer<FoldersProvider>(
+                builder: (BuildContext context, FoldersProvider value,
+                    Widget child) {
+                  if (value.deleteFolderMode) {
+                    return CircularCheckBox(
+                      value: value.isFolderChecked(folder.id),
+                      onChanged: (value) {
+                        Provider.of<FoldersProvider>(context, listen: false)
+                            .toggleFolderDeleteCheckbox(folder.id, value);
+                      },
+                    );
+                  } else {
+                    return Icon(
+                      MaterialCommunityIcons.folder_outline,
+                      color: Theme.of(context).accentColor,
+                    );
+                  }
+                },
+              ),
+            ),
+            //folder name + number of items
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5, top: 10),
+                  child: Text(
+                    folder.name,
+                    style: TextStyle(fontSize: 14),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Opacity(
+                  opacity: 0.6,
+                  child: _getNumberOfItemsText(),
+                ),
+              ],
+            ),
+            //white space
+            Expanded(child: SizedBox()),
+            //popup menu
+            trailing != null
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: trailing,
+                  )
+                : SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getNumberOfItemsText() {
+    final textStyle = TextStyle(fontSize: 12);
+    if (folder.numberOfLists != 1) {
+      return Text(
+        '${folder.numberOfLists} items',
+        style: textStyle,
+      );
+    }
+    return Text(
+      '${folder.numberOfLists} item',
+      style: textStyle,
+    );
+  }
+}
